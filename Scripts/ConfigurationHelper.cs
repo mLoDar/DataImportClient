8a8c3529cdadad48f32109f450a582be9f347db2
mLoDar <mlodarcoding@gmail.com>
2024-12-16 13:08:59 +0100

    Added configuration helper This new class will be used later on to load and save application configuration. Additionally some basic error handling was implemented.

---
+++ Scripts/ConfigurationHelper.cs
@@ -0,0 +1,55 @@
+﻿using DataImportClient.Ressources;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+
+
+
+namespace DataImportClient.Scripts
+{
+    internal class ConfigurationHelper
+    {
+        private static readonly ApplicationSettings.Paths _appPaths = new();
+
+
+
+        internal static async Task<(bool successfullySaved, Exception occurredError)> SaveConfiguration(JObject applicationConfiguration)
+        {
+            try
+            {
+                string configurationFile = _appPaths.configurationFile;
+                string plainTextConfiguration = applicationConfiguration.ToString(Formatting.Indented);
+
+                await File.WriteAllTextAsync(configurationFile, plainTextConfiguration); 
+            }
+            catch (Exception exception)
+            {
+                return (false, exception);
+            }
+
+            return (true, new Exception());
+        }
+
+        internal static async Task<JObject> LoadConfiguration()
+        {
+            JObject loadedConfiguration = [];
+
+            try
+            {
+                string configurationFile = _appPaths.configurationFile;
+                string plainTextConfiguration = await File.ReadAllTextAsync(configurationFile);
+
+                loadedConfiguration = JObject.Parse(plainTextConfiguration);
+            }
+            catch (Exception exception)
+            {
+                loadedConfiguration["error"] = exception.Message;
+                return loadedConfiguration;
+            }
+
+            return loadedConfiguration;
+        }
+    }
+}
