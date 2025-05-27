mLoDar/DataImportClient

commit f4fd41168c4c70f00727ff39e90e99ac29573767

Made additions to the ErrorCache Added new parameters which will be added to an error entry. One of them is a GUID to simply identify each error. The next improvement is a new enum which was added, which holds a category to which an error can belong. With future commits, there will be the improvement that the category for each module will be checked so no error will be sent twice as an email alert.

Modules/ErrorCache.cs
@@ -11,22 +11,37 @@ using Newtonsoft.Json.Linq;
 
 namespace DataImportClient.Modules
 {
+    enum ErrorCategory
+    {
+        ConfigurationFetching,
+        IntegerParsing,
+        SourceFileDataFetching,
+        DatabaseInsertion,
+        FileDeletion,
+        FileMoving,
+        SourceFileDataMinimizing,
+        ApiDataFetching,
+        Miscellaneous,
+    }
+
     struct ErrorCacheEntry
     {
+        internal Guid errorId;
         internal DateTime dateTime;
         internal int processId;
         internal string section;
-        internal string error;
-        internal string detail;
+        internal string errorMessage;
+        internal string errorDetail;
+        internal ErrorCategory errorCategory;
 
         internal readonly string ToMinimalistic()
         {
-            return $"[{section}] - {error}";
+            return $"[{section}] - {errorMessage}";
         }
 
         internal readonly string ToDetailed()
         {
-            return $"[{dateTime:yyyy-MM-dd HH:mm:ss}] - [ProcessId: {processId} | Section: {section}] - {error} {detail}";
+            return $"[{dateTime:yyyy-MM-dd HH:mm:ss}] - [ProcessId: {processId} | Section: {section}] - {errorMessage} {errorDetail}";
         }
     }
 
@@ -38,21 +53,23 @@ namespace DataImportClient.Modules
 
         private static readonly ApplicationSettings.Paths _appPaths = new();
 
-        const int maxEntries = 30;
+        const int maxEntries = 50;
         private readonly object _entriesLock = new();
         private readonly List<ErrorCacheEntry> _entries = [];
 
-        
 
-        internal void AddEntry(string errorSection, string errorMessage, string detailedError)
+
+        internal void AddEntry(string errorSection, string errorMessage, string errorDetail, ErrorCategory errorCategory)
         {
             ErrorCacheEntry entry = new()
             {
+                errorId = Guid.NewGuid(),
                 dateTime = DateTime.Now,
                 processId = Environment.ProcessId,
                 section = errorSection,
-                error = errorMessage,
-                detail = detailedError
+                errorMessage = errorMessage,
+                errorDetail = errorDetail,
+                errorCategory = errorCategory
             };
 
 
@@ -67,12 +84,6 @@ namespace DataImportClient.Modules
                 _entries.Add(entry);
             }
 
-
-
-            if (_entries.Count != 1 && _entries.Count % 10 != 0)
-            {
-                return;
-            }
 
 
             string emailSubject = "Errors at DataImport";

Modules/DistrictHeat.cs
@@ -332,7 +332,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while fetching the settings.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.ConfigurationFetching);
 
                     ImportWorkerLog($"Waiting for {errorTimoutInMilliseconds / 1000} seconds before continuing with the import process.");
 
@@ -351,7 +351,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while assigning variables.";
                     string[] errorDetails = ["Failed to parse 'sourceFileIntervalSeconds' to int."];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.IntegerParsing);
 
                     ImportWorkerLog($"Waiting for {errorTimoutInMilliseconds} seconds before continuing with the import process.");
 
@@ -373,7 +373,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while fetching data from the PLC source file.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.SourceFileDataFetching);
 
                     if (noFilesFound == false)
                     {
@@ -401,7 +401,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while inserting the data into the database.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.DatabaseInsertion);
 
                     if (noFilesFound == false)
                     {
@@ -428,7 +428,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "Failed to delete the source file.";
                     string[] errorDetails = [exception.Message, exception.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.FileDeletion);
                 }
 
                 ImportWorkerLog("Successfully deleted the source file.");
@@ -757,7 +757,7 @@ namespace DataImportClient.Modules
             _dateOfLastLogFileEntry = DateTime.Now.ToString("dd.MM.yyyy - HH:mm:ss");
         }
 
-        private void ThrowModuleError(string errorMessage, string[] errorDetails)
+        private void ThrowModuleError(string errorMessage, string[] errorDetails, ErrorCategory errorCategory)
         {
             ImportWorkerLog($"[ERROR] - {errorMessage}");
 
@@ -769,7 +769,7 @@ namespace DataImportClient.Modules
                 }
             }
 
-            MainMenu._sectionMiscellaneous.errorCache.AddEntry(_currentSection, errorMessage, errorDetails[0]);
+            MainMenu._sectionMiscellaneous.errorCache.AddEntry(_currentSection, errorMessage, errorDetails[0], errorCategory);
 
             State = ModuleState.Error;
             _errorCount++;
@@ -796,7 +796,7 @@ namespace DataImportClient.Modules
             {
                 string errorMessage = "Failed to move the current source file.";
                 string[] errorDetails = [exception.Message, exception.InnerException?.ToString() ?? string.Empty, $"File path: {_currentSourceFilePath}."];
-                ThrowModuleError(errorMessage, errorDetails);
+                ThrowModuleError(errorMessage, errorDetails, ErrorCategory.FileMoving);
             }
         }
     }

Modules/Electricity.cs
@@ -335,7 +335,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while fetching the settings.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.ConfigurationFetching);
 
                     ImportWorkerLog($"Waiting for {errorTimoutInMilliseconds / 1000} seconds before continuing with the import process.");
 
@@ -354,7 +354,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while assigning variables.";
                     string[] errorDetails = ["Failed to parse 'sourceFileIntervalSeconds' to int."];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.IntegerParsing);
 
                     ImportWorkerLog($"Waiting for {errorTimoutInMilliseconds} seconds before continuing with the import process.");
 
@@ -376,7 +376,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while fetching data from the PLC source file.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.SourceFileDataFetching);
 
                     if (noFilesFound == false)
                     {
@@ -401,7 +401,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while minimizing the fetched data.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.SourceFileDataMinimizing);
 
                     MoveSourceFileToFaultyFilesFolder();
 
@@ -427,7 +427,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while inserting the data into the database.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.DatabaseInsertion);
 
                     MoveSourceFileToFaultyFilesFolder();
 
@@ -451,7 +451,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "Failed to delete the source file.";
                     string[] errorDetails = [exception.Message, exception.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.FileDeletion);
                 }
 
                 ImportWorkerLog("Successfully deleted the source file.");
@@ -1017,7 +1017,7 @@ namespace DataImportClient.Modules
             _dateOfLastLogFileEntry = DateTime.Now.ToString("dd.MM.yyyy - HH:mm:ss");
         }
 
-        private void ThrowModuleError(string errorMessage, string[] errorDetails)
+        private void ThrowModuleError(string errorMessage, string[] errorDetails, ErrorCategory errorCategory)
         {
             ImportWorkerLog($"[ERROR] - {errorMessage}");
 
@@ -1029,7 +1029,7 @@ namespace DataImportClient.Modules
                 }
             }
 
-            MainMenu._sectionMiscellaneous.errorCache.AddEntry(_currentSection, errorMessage, errorDetails[0]);
+            MainMenu._sectionMiscellaneous.errorCache.AddEntry(_currentSection, errorMessage, errorDetails[0], errorCategory);
 
             State = ModuleState.Error;
             _errorCount++;
@@ -1056,7 +1056,7 @@ namespace DataImportClient.Modules
             {
                 string errorMessage = "Failed to move the current source file.";
                 string[] errorDetails = [exception.Message, exception.InnerException?.ToString() ?? string.Empty, $"File path: {_currentSourceFilePath}."];
-                ThrowModuleError(errorMessage, errorDetails);
+                ThrowModuleError(errorMessage, errorDetails, ErrorCategory.FileMoving);
             }
         }
     }

Modules/Photovoltaic.cs
@@ -345,7 +345,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while fetching the settings.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.ConfigurationFetching);
 
                     ImportWorkerLog($"Waiting for {errorTimoutInMilliseconds / 1000} seconds before continuing with the import process.");
 
@@ -361,7 +361,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while assigning variables.";
                     string[] errorDetails = ["Failed to parse 'apiIntervalSeconds' to int."];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.IntegerParsing);
 
                     ImportWorkerLog($"Waiting for {errorTimoutInMilliseconds} seconds before continuing with the import process.");
 
@@ -379,7 +379,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while fetching data from the API provider.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.ApiDataFetching);
 
                     ImportWorkerLog($"Waiting for {errorTimoutInMilliseconds} seconds before continuing with the import process.");
 
@@ -399,7 +399,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while inserting the data into the database.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.DatabaseInsertion);
 
                     ImportWorkerLog($"Waiting for {errorTimoutInMilliseconds} seconds before continuing with the import process.");
 
@@ -750,7 +750,7 @@ namespace DataImportClient.Modules
             _dateOfLastLogFileEntry = DateTime.Now.ToString("dd.MM.yyyy - HH:mm:ss");
         }
 
-        private void ThrowModuleError(string errorMessage, string[] errorDetails)
+        private void ThrowModuleError(string errorMessage, string[] errorDetails, ErrorCategory errorCategory)
         {
             ImportWorkerLog($"[ERROR] - {errorMessage}");
 
@@ -762,7 +762,7 @@ namespace DataImportClient.Modules
                 }
             }
 
-            MainMenu._sectionMiscellaneous.errorCache.AddEntry(_currentSection, errorMessage, errorDetails[0]);
+            MainMenu._sectionMiscellaneous.errorCache.AddEntry(_currentSection, errorMessage, errorDetails[0], errorCategory);
 
             State = ModuleState.Error;
             _errorCount++;

Modules/Weather.cs
@@ -344,7 +344,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while fetching the settings.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.ConfigurationFetching);
 
                     ImportWorkerLog($"Waiting for {errorTimoutInMilliseconds / 1000} seconds before continuing with the import process.");
 
@@ -366,7 +366,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while assigning variables.";
                     string[] errorDetails = ["Failed to parse 'apiIntervalSeconds' to int."];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.IntegerParsing);
 
                     ImportWorkerLog($"Waiting for {errorTimoutInMilliseconds} seconds before continuing with the import process.");
 
@@ -384,7 +384,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while fetching data from the API provider.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.ApiDataFetching);
 
                     ImportWorkerLog($"Waiting for {errorTimoutInMilliseconds} seconds before continuing with the import process.");
 
@@ -404,7 +404,7 @@ namespace DataImportClient.Modules
                 {
                     string errorMessage = "An error has occurred while inserting the data into the database.";
                     string[] errorDetails = [occurredError.Message, occurredError.InnerException?.ToString() ?? string.Empty];
-                    ThrowModuleError(errorMessage, errorDetails);
+                    ThrowModuleError(errorMessage, errorDetails, ErrorCategory.DatabaseInsertion);
 
                     ImportWorkerLog($"Waiting for {errorTimoutInMilliseconds} seconds before continuing with the import process.");
 
@@ -656,7 +656,7 @@ namespace DataImportClient.Modules
             _dateOfLastLogFileEntry = DateTime.Now.ToString("dd.MM.yyyy - HH:mm:ss");
         }
 
-        private void ThrowModuleError(string errorMessage, string[] errorDetails)
+        private void ThrowModuleError(string errorMessage, string[] errorDetails, ErrorCategory errorCategory)
         {
             ImportWorkerLog($"[ERROR] - {errorMessage}");
 
@@ -668,7 +668,7 @@ namespace DataImportClient.Modules
                 }
             }
 
-            MainMenu._sectionMiscellaneous.errorCache.AddEntry(_currentSection, errorMessage, errorDetails[0]);
+            MainMenu._sectionMiscellaneous.errorCache.AddEntry(_currentSection, errorMessage, errorDetails[0], errorCategory);
 
             State = ModuleState.Error;
             _errorCount++;

commit e85d96d30540ff5e5e87b91eed4bb0e3d0ef6837

Improved error cache and email alerts With this commit there were some big changes regarding the email alerts within the error cache. Now there will be made use of the error categories which were introduced in commit f4fd41168c4c70f00727ff39e90e99ac29573767. Email will be sent from now on at earliest every 300 seconds. Additionally there will be no further email alerts for category x if this specifc category/module combination was already sent.

EntryPoint.cs
@@ -225,7 +225,6 @@ namespace DataImportClient
                         ["emailRecipients"] = new JArray()
                         {
                             "example1@email.com",
-                            "example2@email.com",
                         },
                     };
 

Modules/ErrorCache.cs
@@ -53,15 +53,19 @@ namespace DataImportClient.Modules
 
         private static readonly ApplicationSettings.Paths _appPaths = new();
 
-        const int maxEntries = 50;
+        private const int _maxEntries = 50;
         private readonly object _entriesLock = new();
-        private readonly List<ErrorCacheEntry> _entries = [];
+        private readonly List<ErrorCacheEntry> _errorCache = [];
+        private readonly static List<ErrorCacheEntry> _errorAlerted = [];
+
+        private const int _emailAlertsCooldownSeconds = 300;
+        private static long _lastEmailAlertUnixSeconds = 0;
 
 
 
         internal void AddEntry(string errorSection, string errorMessage, string errorDetail, ErrorCategory errorCategory)
         {
-            ErrorCacheEntry entry = new()
+            ErrorCacheEntry newEntry = new()
             {
                 errorId = Guid.NewGuid(),
                 dateTime = DateTime.Now,
@@ -76,20 +80,35 @@ namespace DataImportClient.Modules
 
             lock (_entriesLock)
             {
-                if (_entries.Count + 1 > maxEntries)
+                if (_errorCache.Count + 1 > _maxEntries)
                 {
-                    _entries.RemoveAt(0);
+                    _errorCache.RemoveAt(0);
                 }
 
-                _entries.Add(entry);
-            }
+                _errorCache.Add(newEntry);
+
+                ActivityLogger.Log(_currentSection, "Received a new error for the error cache. Checking if the application needs to send an email alert.");
+                ActivityLogger.Log(_currentSection, $"ErrorCategory: {newEntry.errorCategory} | ErrorModule: {newEntry.section}", true);
+
+                bool sendAlertEmail = ShouldSendEmailAlert(newEntry);
+
+                if (sendAlertEmail == false)
+                {
+                    ActivityLogger.Log(_currentSection, "Suppressed sending of a new email alert.");
+                    return;
+                }
+
+                ActivityLogger.Log(_currentSection, "Sending a new email alert for the current error.");
 
 
 
-            string emailSubject = "Errors at DataImport";
-            string emailBody = $"There {(_entries.Count > 1 ? "are" : "is")} currently {_entries.Count} error{(_entries.Count > 1 ? string.Empty : "s")} which need{(_entries.Count > 1 ? "s" : string.Empty)} a manual review.";
+                string emailSubject = "Fehler beim Daten-Import";
+                string emailBody = $"Ein neuer Fehler des Typs '{newEntry.errorCategory}' ist im Bereich '{newEntry.section}' aufgetreten." +
+                             $"\r\nDerzeit {(_errorCache.Count > 1 ? "sind" : "ist")} {_errorCache.Count} Fehler im Fehlerspeicher des DataImportClient." +
+                         $"\r\n\r\nGenauere Details werden in dem Modul-Spezifischem Logfile aufgelistet." + 
+                             $"\r\nBenachrichtigungen dieser Art können im DataImportClient unter dem Menüpunkt 'Miscellaneous' deaktiviert werden.";
 
-            Task.Run(async () =>
+                Task.Run(async () =>
                 {
                     JObject savedConfiguration;
 
@@ -136,14 +155,17 @@ namespace DataImportClient.Modules
                         ActivityLogger.Log(_currentSection, "Please check the error logs above to fix this error.", true);
                     }
                 }
-            );
+                );
+
+                _errorAlerted.Add(newEntry);
+            }
         }
 
         internal void RemoveSectionFromCache(string errorSection)
         {
             List<ErrorCacheEntry> entriesForRemoval = [];
 
-            foreach (ErrorCacheEntry entry in _entries)
+            foreach (ErrorCacheEntry entry in _errorCache)
             {
                 if (entry.section.Equals(errorSection))
                 {
@@ -153,15 +175,22 @@ namespace DataImportClient.Modules
 
             foreach (ErrorCacheEntry entry in entriesForRemoval)
             {
-                _entries.Remove(entry);
+                _errorCache.Remove(entry);
             }
+
+            foreach (ErrorCacheEntry entry in entriesForRemoval)
+            {
+                _errorAlerted.Remove(entry);
+            }
+
+            ActivityLogger.Log(_currentSection, $"Removed all error for the section '{errorSection}' from the error cache!");
         }
 
         internal void DisplayMinimalistic()
         {
             int cacheViewStartIndex = 0;
 
-            if (_entries.Count <= 0)
+            if (_errorCache.Count <= 0)
             {
                 ActivityLogger.Log(_currentSection, "The error cache currently does not hold any entries.");
 
@@ -174,7 +203,7 @@ namespace DataImportClient.Modules
 
 
             ActivityLogger.Log(_currentSection, "Displaying a minimal error cache, waiting for return signal.");
-            ActivityLogger.Log(_currentSection, $"The error cache currently holds {(_entries.Count > 1 ? "1 entry" : $"{_entries.Count} entries")}.");
+            ActivityLogger.Log(_currentSection, $"The error cache currently holds {(_errorCache.Count > 1 ? "1 entry" : $"{_errorCache.Count} entries")}.");
 
 
 
@@ -203,11 +232,11 @@ namespace DataImportClient.Modules
             int scrollBarHelper = 0;
             int errorsDisplayedAtOnce = 10;
 
-            var (scrollBarStart, scrollBarEnd) = GetScrollbarRange(_entries.Count, errorsDisplayedAtOnce, cacheViewStartIndex);
+            var (scrollBarStart, scrollBarEnd) = GetScrollbarRange(_errorCache.Count, errorsDisplayedAtOnce, cacheViewStartIndex);
 
-            for (int currentErrorIndex = cacheViewStartIndex; currentErrorIndex < cacheViewStartIndex + errorsDisplayedAtOnce && currentErrorIndex < _entries.Count; currentErrorIndex++)
+            for (int currentErrorIndex = cacheViewStartIndex; currentErrorIndex < cacheViewStartIndex + errorsDisplayedAtOnce && currentErrorIndex < _errorCache.Count; currentErrorIndex++)
             {
-                string currentError = _entries[currentErrorIndex].ToMinimalistic();
+                string currentError = _errorCache[currentErrorIndex].ToMinimalistic();
 
                 if (currentError.Length > maxErrorLength)
                 {
@@ -242,7 +271,7 @@ namespace DataImportClient.Modules
             switch (pressedKey)
             {
                 case ConsoleKey.DownArrow:
-                    if (cacheViewStartIndex + 1 <= _entries.Count - errorsDisplayedAtOnce)
+                    if (cacheViewStartIndex + 1 <= _errorCache.Count - errorsDisplayedAtOnce)
                     {
                         cacheViewStartIndex += 1;
                     }
@@ -274,7 +303,7 @@ namespace DataImportClient.Modules
 
         internal async Task DisplayDetailed()
         {
-            if (_entries.Count <= 0)
+            if (_errorCache.Count <= 0)
             {
                 ActivityLogger.Log(_currentSection, "The error cache currently does not hold any entries.");
 
@@ -293,12 +322,12 @@ namespace DataImportClient.Modules
             List<string> errorCacheEntries = [];
 
             DateTime currentTime = DateTime.UtcNow;
-            long unixTime = ((DateTimeOffset)currentTime).ToUnixTimeSeconds();
+            long unixTimeSeconds = ((DateTimeOffset)currentTime).ToUnixTimeSeconds();
 
-            string exportFileName = Path.Combine(_appPaths.clientFolder, $"errorCacheExport-{unixTime}.txt");
+            string exportFileName = Path.Combine(_appPaths.clientFolder, $"errorCacheExport-{unixTimeSeconds}.txt");
 
 
-            foreach (ErrorCacheEntry entry in _entries)
+            foreach (ErrorCacheEntry entry in _errorCache)
             {
                 errorCacheEntries.Add(entry.ToDetailed());
             }
@@ -391,6 +420,36 @@ namespace DataImportClient.Modules
             int scrollBarEnd = scrollBarStart + scrollBarHeight;
 
             return (scrollBarStart, scrollBarEnd);
+        }
+
+        private static bool ShouldSendEmailAlert(ErrorCacheEntry newEntry)
+        {
+            DateTime currentTime = DateTime.UtcNow;
+            long unixTimeSeconds = ((DateTimeOffset)currentTime).ToUnixTimeSeconds();
+
+            if (unixTimeSeconds - _lastEmailAlertUnixSeconds <= _emailAlertsCooldownSeconds)
+            {
+                ActivityLogger.Log(_currentSection, $"[WARNING] - Last email was sent less than {_emailAlertsCooldownSeconds} seconds ago.", true);
+                return false;
+            }
+
+            foreach (ErrorCacheEntry alertedEntry in _errorAlerted)
+            {
+                bool matchingCategory = alertedEntry.errorCategory == newEntry.errorCategory;
+                bool matchingSection = alertedEntry.section == newEntry.section;
+
+                if (matchingCategory && matchingSection)
+                {
+                    ActivityLogger.Log(_currentSection, $"[WARNING] - This specific category/module configuration was already sent as an email alert.", true);
+                    return false;
+                }
+            }
+
+            ActivityLogger.Log(_currentSection, $"This specific category/module configuration was not sent previously as an email alert.", true);
+
+            _lastEmailAlertUnixSeconds = unixTimeSeconds;
+
+            return true;
         }
     }
 }